b15dbc546e2ee18aa816f871e67af7dfc5f565d1
sidneinas <53159879+Sid-srn@users.noreply.github.com>
2024-04-04 08:03:06 -0300

    update exercicio alcol gasolina

--- Exercicios/alcool_gasolina/app/src/main/java/com/example/alcool_gasolina/MainActivity.kt
+++ Exercicios/alcool_gasolina/app/src/main/java/com/example/alcool_gasolina/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.alcool_gasolina
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import com.example.alcool_gasolina.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bind: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
 
-        val btnCalculat = findViewById<Button>(R.id.button)
-        val editAlcool = findViewById<EditText>(R.id.)
+        //val btnCalculat = findViewById<Button>(R.id.button)
+        //val editAlcool = findViewById<EditText>(R.id.)
+
+        bind.button.setOnClickListener {
+            val calcAlcool = bind.editTextAlcool.text.toString().toFloat()
+            val calcGas = bind.editTextAlcool.text.toString().toFloat()
+            val result = calculoAG(calcGas,calcAlcool)
+            if (result>0.7){
+                bind.txtResultado.text = "Abasteça com alcool"
+            }else{
+                bind.txtResultado.text = "Abasteça com gasolina"
+            }
+        }
     }
-    //fun calculoAG(gasolina:Int, alccol:Int) =  alccol/gasolina
-    fun calculoAG(gasolina:Int, alccol:Int):Int{
+    //fun calculoAG(gasolina:Int, alcool:Int) =  alccol/gasolina
+    fun calculoAG(gasolina:Float, alccol:Float):Float{
         return alccol/gasolina
     }
 }
